fdec91bb1f35a570392cbcf1c299f3738ed58b55
Renato Castro <renatocastro02@hotmail.com>
2021-07-02 14:33:33 -0300

    refactor: rename Encrypter to Hasher

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,4 +1,4 @@
-export * from './criptography/encrypter';
+export * from './criptography/hasher';
 export * from './db/add-account-repository';
 export * from './db/log-error-repository';
 export * from './criptography/hash-comparer';
--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/models';
 export * from '../../../domain/usecases';
 export * from '../../protocols';
+export * from '../../protocols/criptography/hasher';
