5b34069c41482c20ef8ee1083a6cc864bd1a5fce
Renato Castro <renatocastro02@hotmail.com>
2021-07-16 15:40:45 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
         })
         .expect(200);
     });
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          email: 're@re.com',
+          password: '123',
+        })
+        .expect(401);
+    });
   });
 });
